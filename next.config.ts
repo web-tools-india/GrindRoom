475bbb0a9b92ccbb8a7163c575a28aa775f63fd9
web-tools-india <nishantborse.2008@gmail.com>
2026-03-11 14:20:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["grindroom.pages.dev"],
+    },
+  },
 };
 
 export default nextConfig;
